49f1f257ef00de1b7b52ca3d80398798c3c6a1aa
Matt Camilli <mlcamilli@gmail.com>
2015-05-04 15:15:56 -0400

    added packages

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='django-multiple-choice-field',
     author="TrackMaven Engineering",
     author_email="engineering@trackmaven.com",
-    version='0.0.2',
+    version='0.0.3',
     description="",
     url="https://github.com/TrackMaven/django-multiple-choice-field",
     license="MIT",
+    packages=find_packages(exclude=['tests']),
 )
