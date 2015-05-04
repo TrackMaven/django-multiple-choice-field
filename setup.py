9dbbe9a2ab51a158103cb4136d06be49232903d5
Matt Camilli <mlcamilli@gmail.com>
2015-05-04 14:32:55 -0400

    renamed package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='django-multiple-choice-field',
     author="TrackMaven Engineering",
     author_email="engineering@trackmaven.com",
-    version='0.0.1',
+    version='0.0.2',
     description="",
     url="https://github.com/TrackMaven/django-multiple-choice-field",
     license="MIT",
