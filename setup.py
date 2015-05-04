526bf9bcd8e0f66679e0445d78764bfd4f37cf1e
Matt Camilli <mlcamilli@gmail.com>
2015-05-04 13:32:11 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='django-multiple-choice-field',
+    author="TrackMaven Engineering",
+    author_email="engineering@trackmaven.com",
+    version='0.0.1',
+    description="",
+    url="https://github.com/TrackMaven/django-multiple-choice-field",
+    license="MIT",
+)
